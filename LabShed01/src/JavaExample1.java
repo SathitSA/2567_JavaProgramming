589428811be5a6985bd91062f64ec743a30e657c
SathitSA <Sathitsaichu@gmail.com>
2024-11-27 10:14:31 +0700

    JavaExample

---
+++ LabShed01/src/JavaExample1.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.text.*;
+
+public class JavaExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//กำหนดobject
+		Scanner input = new Scanner(System.in);
+		//กำหนดobjectในการจัดรูปแบบเลข
+		DecimalFormat frm = new DecimalFormat();
+		//รับและหมายเหตุ ชื่อสิรค้า
+		System.out.print("input product name: ");
+		String productName = input.nextLine();
+		//แสดงและรับข้อมูลสินค้า
+		System.out.print("Input product unit: ");
+		int productUnit = input.nextInt();
+		System.out.print("Input price per unit: ");
+		float productPrice = input.nextFloat();
+		
+		System.out.println();
+		//คำนวนหาผลรวมของสินค้า
+		float totalPriceofProduct = productUnit * productPrice;
+		System.out.println("Total Price is "+frm.format(totalPriceofProduct) +" baht");
+		//คำนวณภาษีเพิ่ม 7%
+				//float totalwithVat = totalPriceofProduct + (totalPriceofProduct*7/100);
+				float totalwithVat = totalPriceofProduct * 1.07f;
+				System.out.println("Add VAT 7% is "+ frm.format(totalwithVat) + " baht.");
+				System.out.println(); //การเว้น 1 บรรทัด
+				//Output using String.format and printf
+				String frmtotalwithVat = String.format("%,.2f",totalwithVat);
+				System.out.println("Output using String.format");
+				System.out.println("Add VAT 7% is "+ frmtotalwithVat + " baht.");	
+				System.out.println(); //การเว้น 1 บรรทัด
+				System.out.println("Output using printf");
+				System.out.printf("Add VAT 7%%  is %,.2f ",  totalwithVat);		
+				input.close();
+		
+		
+		input.close();
+	}
+
+}
---
+++ LabShed01/src/JavaExmple2.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+public class JavaExmple2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String productName = JOptionPane.showInputDialog("Input product Name");
+		String strProductUnit = JOptionPane.showInputDialog("Input product Unit");
+		int ProductUnit = Integer.parseInt(strProductUnit);
+		double productPrict = Double.parseDouble(JOptionPane.showInputDialog("Input Prict per Unit"));
+		
+		Double totalPriceofProduct = ProductUnit * productPrict;
+		double totalwithVat = totalPriceofProduct * 1.07;
+		String frmtotalPriceofProduct = String.format("%,.2f",totalwithVat);
+		String frmtotalwithVat = String.format("%,.2f",totalwithVat);
+		JOptionPane.showMessageDialog(null,"Total price is" +totalPriceofProduct+" baht."+"\nAdd VAT7% is"+totalwithVat+ " baht.");
+		 
+	}
+
+}
