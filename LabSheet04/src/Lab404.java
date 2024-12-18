5a8a621c0ba59e3bd04616eb3f1b4d784fd65deb
SathitSA <Sathitsaichu@gmail.com>
2024-12-18 11:23:04 +0700

    LabSheet04practice

--- LabSheet04/src/Lab404.java
+++ LabSheet04/src/Lab404.java
@@ -15,7 +15,7 @@ public class Lab404 {
 		isStudentIDValid = isLenght(StudenID,10);
 		isStudentCodeValid = isLenght(subjectCode,7);
 		
-		while(StudenID.length() != 10 && subjectCode.length() != 7 ) {
+		while(!isStudentIDValid && !isStudentCodeValid ) {
 			System.out.print("Enter student ID (10 digite): ");
 			StudenID = scan.nextLine();
 			System.out.print("Enter subject code (7 digite): ");
@@ -25,17 +25,40 @@ public class Lab404 {
 		}
 		
 		if(isStudentIDValid && isStudentCodeValid) {
-			//boolean isITStudent = isITStudent(StudenID);
-			//boolean isITSubject = isITSubject(subjectCode);
+			boolean isITStudent = isITStudent(StudenID);
+			boolean isITSubject = isITSubject(subjectCode);
 			
 			
-			//displayData(isITStudent,isITSubject);
+			displayData(isITStudent,isITSubject);
 		}
 
 	}
 	
 	public static boolean isLenght(String input,int len) {
 		return input.length() == len;
+	}
+	public static boolean isITStudent(String SID) {
+		String yearStudy = SID.substring(3);
+		String isIT = SID.substring(4,6);
+		if(yearStudy == "1" && isIT == "311") {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public static boolean isITSubject(String SBJ) {
+		String NumSubject = SBJ.substring(1,2);
+		String YearSub = SBJ.substring(5);
+		if(NumSubject == "21" && YearSub == "1") {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public static void displayData(boolean ITStud,boolean ITSub) {
+		
 	}
 
 }
