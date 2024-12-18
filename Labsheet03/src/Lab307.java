f6f49110cfa762ecd502c476f99a22c46160fab3
SathitSA <Sathitsaichu@gmail.com>
2024-12-18 08:39:09 +0700

    LaSheet03

---
+++ Labsheet03/src/Lab307.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+public class Lab307 {
+
+	public static void main(String[] args) {
+			Scanner scan = new Scanner (System.in);
+			String text = "";
+			String palindromeCk = "";
+			System.out.print("Text: ");
+			text = scan.nextLine().trim().replace(" ", "").toLowerCase();
+			for(int i = text.length()-1;i>=0;i--) {
+				char ch = text.charAt(i);
+				palindromeCk+=ch;
+			}
+			
+			
+			
+			if(palindromeCk.equals(text)) {
+				System.out.println("It is palindrom");
+			}else {
+				System.out.println("It is not palindrom");
+			}
+		}
+
+	}
